55a149d4882966139babd72f6df8b753049f29df
valentin kueck <kueck.valentin@googlemail.com>
2026-01-31 22:31:57 +0100

    Remove unnecessary scroll behavior from HTML tag in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="de" className="overflow-y-scroll">
+    <html lang="de">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
